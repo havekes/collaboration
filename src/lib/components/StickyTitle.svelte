9b3df2c7128aea74194a633888a5c8d4400ff067
Greg Havekes <883845+havekes@users.noreply.github.com>
2025-12-21 20:59:53 -0800

    fix bad shadow

--- src/lib/components/StickyTitle.svelte
+++ src/lib/components/StickyTitle.svelte
@@ -9,6 +9,6 @@
 
 <div class="sticky top-29 w-full bg-cornsilk-50/50 py-4 text-xl backdrop-blur-xl transition-all">
 	<div class="container m-auto">
-		<h1 class="font-bold text-cornsilk-900 text-shadow-xs">{@render children()}</h1>
+		<h1 class="font-bold text-cornsilk-900">{@render children()}</h1>
 	</div>
 </div>
