83e8a3ec06fc6b9d0b52c09b0504753e2ad60f30
Greg Havekes <883845+havekes@users.noreply.github.com>
2025-12-22 17:13:41 -0800

    add references

---
+++ src/lib/components/Reference.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	type Props = {
+		link: string;
+		children: Snippet;
+	};
+
+	let { link, children }: Props = $props();
+</script>
+
+<p class="ml-12 -indent-12" id={link}>
+	{@render children()}
+	<a href={link} class="text-cornsilk-700 underline underline-offset-2 hover:text-cornsilk-900"
+		>{link}</a
+	>
+</p>
---
+++ src/routes/references/+page.svelte
@@ -0,0 +1,93 @@
+<script>
+	import Reference from '$lib/components/Reference.svelte';
+</script>
+
+<svelte:head>
+	<title>Referencess</title>
+</svelte:head>
+
+<div class="my-8 flex flex-col items-center space-y-8">
+	<section class="container">
+		<h1 class="text-3xl font-bold text-cornsilk-800">References</h1>
+	</section>
+
+	<section class="container space-y-8">
+		<Reference link="https://doi.org/10.33596/coll.123">
+			Bustos, T. E., Simkani, S., Sridhar, A., & Drahota, A. (2024). “Good Solid Relationships Make
+			Programs Work”: A Mixed Methods Assessment of Determinants to Community Research Partnerships
+			in Flint, MI. <span class="italic">A Journal of Community-Based Research and Practice, 7</span
+			>(1).
+		</Reference>
+		<Reference link="https://doi.org/10.3390/curroncol31110513">
+			Farrell, A. C., Lawson, J. A., Ovarian Cancer Canada’s Patient Partners in Research Team,
+			Ross, A., & Tone, A. A. (2024). Advancing Research Alongside Patient Partners: Next-Generation
+			Best Practices for Effective Collaboration in Health Research. <span class="italic"
+				>Current Oncology, 31</span
+			>(11), 6956–6978.
+		</Reference>
+		<Reference link="https://doi.org/10.1186/s12889-015-1949-1">
+			Jagosh, J., Bush, P. L., Salsberg, J., Macaulay, A. C., Greenhalgh, T., Wong, G., Cargo, M.,
+			Green, L. W., Herbert, C. P., & Pluye, P. (2015). A realist evaluation of community-based
+			participatory research: Partnership synergy, trust building and related ripple effects. <span
+				class="italic">BMC Public Health, 15</span
+			>(1), 725.
+		</Reference>
+		<Reference link="http://dx.doi.org/10.5772/intechopen.95396">
+			Kato, D., & Suzuki, M. (2022). Rolefulness and Interpersonal Relationships. In <span
+				class="italic">Interpersonal Relationship</span
+			> (1st ed., pp. 21-32). IntechOpen.
+		</Reference>
+		<Reference link="https://doi.org/10.1002/fsh.10536">
+			Newcomb, T. J., Simonin, P. W., Martinez, F. A., Chadderton, W. L., Bossenbroek, J. M.,
+			Cudmore, B., Hoff, M. H., Keller, R. P., Ridenhour, B. D., Rothlisberger, J. D., Rutherford,
+			E. S., Van Egeren, S., & Lodge, D. M. (2021). A Best Practices Case Study for Scientific
+			Collaboration between Researchers and Managers. <span class="italic">Fisheries, 46</span>(3),
+			131–138.
+		</Reference>
+		<Reference link="https://doi.org/10.1186/s12939-022-01663-y">
+			Oetzel, J. G., Boursaw, B., Magarati, M., Dickson, E., Sanchez-Youngman, S., Morales, L.,
+			Kastelic, S., Eder, M. “Mickey,” & Wallerstein, N. (2022). Exploring theoretical mechanisms of
+			community-engaged research: A multilevel cross-sectional national study of structural and
+			relational practices in community-academic partnerships. <span class="italic"
+				>International Journal for Equity in Health, 21</span
+			>(1), 59.
+		</Reference>
+		<Reference link="https://doi.org/10.1080/0142159X.2022.2057287">
+			Olmos-Vega, F. M., Stalmeijer, R. E., Varpio, L., & Kahlke, R. (2022). A practical guide to
+			reflexivity in qualitative research: AMEE Guide No. 149. <span class="italic"
+				>Medical Teacher</span
+			>, 1–11.
+		</Reference>
+		<Reference link="https://doi.org/10.1136/bmjgh-2024-015497">
+			Ozano, K., Alam, W., Aktar, B., Okoth, L., Chumo, I., Quach, J. A., Muturi, N., Saidu, S.,
+			Gandi, I., Georgi, N. W., Otiso, L., Conteh, A., Theobald, S., Dean, L., Tolhurst, R., Karuga,
+			R., Karki, J., Garimella, S., Rao, V., … Steege, R. (2024). Seven core competencies and
+			conditions for equitable partnerships and power sharing in community-based participatory
+			research. <span class="italic">BMJ Global Health, 9</span>(11).
+		</Reference>
+		<Reference link="https://doi.org/10.1186/s12909-021-02892-5">
+			Tan, X. H., Foo, M. A., Lim, S. L. H., Lim, M. B. X. Y., Chin, A. M. C., Zhou, J., Chiam, M.,
+			& Krishna, L. K. R. (2021). Teaching and assessing communication skills in the postgraduate
+			medical setting: A systematic scoping review. <span class="italic"
+				>BMC Medical Education, 21</span
+			>, 1–19.
+		</Reference>
+		<Reference link="https://doi.org/10.3389/fcomm.2019.00002">
+			Walker, G. B., & Daniels, S. E. (2019). Collaboration in Environmental Conflict Management and
+			Decision-Making: Comparing Best Practices With Insights From Collaborative Learning Work. <span
+				class="italic">Frontiers in Communication, 4</span
+			>.
+		</Reference>
+		<Reference link="https://doi.org/10.3389/fcomm.2023.1228650">
+			Walker, G., Severson, G., & Daniels, S. (2023). Collaborative Alignment: A framework for
+			community-based collaboration for natural resource management, environmental policy decisions,
+			and locally-led climate action. <span class="italic">Frontiers in Communication, 8</span>,
+			1228650.
+		</Reference>
+		<Reference link="https://doi.org/10.1097/MLR.0000000000000953">
+			Wilkins, C. H. (2018). Effective Engagement Requires Trust and Being Trustworthy. <span
+				class="italic">Medical Care, 56</span
+			>(Suppl 1), S6–S8.
+		</Reference>
+	</section>
+</div>
